cbc993bcf7ef3d8407e33c699f3b69875520a638
NataliaNikitina <nne2418@gmail.com>
2024-03-02 18:29:12 -0800

    Initial Commit

---
+++ BarcodeTextScanner/AppViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  AppViewModel.swift
+//  BarcodeTextScanner
+//
+//  Created by Tasha N on 02.03.2024.
+//
+
+import Foundation
+import SwiftUI
+
+enum DataScannerAccessStatusType {
+    case notDetermined
+    case cameraAccessNotGranted
+    case cameraNotAvailable
+    case scannerAvailable
+    case scannerNotAvailable
+    
+}
+
+final class AppViewModel: ObservedObject {
+    
+    
+}
---
+++ BarcodeTextScanner/BarcodeTextScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeTextScannerApp.swift
+//  BarcodeTextScanner
+//
+//  Created by Tasha N on 02.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeTextScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
